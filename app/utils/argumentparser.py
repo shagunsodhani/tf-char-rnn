938c674a907d662b5a66c5735435a240696e0519
shagun Sodhani <sshagunsodhani@gmail.com>
2016-08-25 22:35:51 +0530

    Added argumentparser

---
+++ app/utils/argumentparser.py
@@ -0,0 +1,38 @@
+from __future__ import print_function
+from six import text_type
+import argparse
+
+
+def ArgumentParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_dir', type=str, default='input/shakespeare',
+                        help='input directory containing data.txt')
+    parser.add_argument('--model_dir', type=str, default='model',
+                        help='directory to store checkpointed models')
+    parser.add_argument('--rnn_size', type=int, default=128,
+                        help='size of RNN hidden state')
+    parser.add_argument('--num_layers', type=int, default=2,
+                        help='number of layers in the RNN')
+    parser.add_argument('--model', type=str, default='lstm',
+                        help='rnn, gru, or lstm')
+    parser.add_argument('--batch_size', type=int, default=50,
+                        help='minibatch size')
+    parser.add_argument('--seq_length', type=int, default=100,
+                        help='RNN sequence length')
+    parser.add_argument('--num_epochs', type=int, default=100,
+                        help='number of epochs')
+    parser.add_argument('--save_frequency', type=int, default=10000,
+                        help='save frequency')
+    parser.add_argument('--grad_clip', type=float, default=5.,
+                        help='clip gradients at this value')
+    parser.add_argument('--learning_rate', type=float, default=0.1,
+                        help='learning rate')
+    parser.add_argument('--decay_rate', type=float, default=0.97,
+                        help='decay rate for rmsprop')
+    parser.add_argument('-n', type=int, default=500,
+                        help='number of characters to sample')
+    parser.add_argument('--prime', type=text_type, default=u'a',
+                        help='prime text')
+    parser.add_argument('--sample', type=int, default=1,
+                        help='0 to use max at each timestep, 1 to sample at each timestep, 2 to sample on spaces')
+    return parser.parse_args()
